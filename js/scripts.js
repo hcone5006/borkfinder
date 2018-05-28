a714791361923ccb62218933b95b84b6c353117a
HeidiC <heidic@traffic.net.nz>
2018-05-28 20:59:32 +1200

    working on search listing page

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
 
-    // Carousel functions
-    $("#mycarousel").carousel( { interval: 2000 } );
-    $("#carousel-pause").click(function(){
-        $("#mycarousel").carousel('pause');
-    });
-    $("#carousel-play").click(function(){
-        $("#mycarousel").carousel('cycle');
-    });
-    $("#carouselButton").click(function(){
-        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-            $("#mycarousel").carousel('pause');
-            $("#carouselButton").children("span").removeClass('fa-pause');
-            $("#carouselButton").children("span").addClass('fa-play');
-        }
-        else if ($("#carouselButton").children("span").hasClass('fa-play')){
-            $("#mycarousel").carousel('cycle');
-            $("#carouselButton").children("span").removeClass('fa-play');
-            $("#carouselButton").children("span").addClass('fa-pause');                    
-        }
-    });
+    // // Carousel functions
+    // $("#mycarousel").carousel( { interval: 2000 } );
+    // $("#carousel-pause").click(function(){
+    //     $("#mycarousel").carousel('pause');
+    // });
+    // $("#carousel-play").click(function(){
+    //     $("#mycarousel").carousel('cycle');
+    // });
+    // $("#carouselButton").click(function(){
+    //     if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+    //         $("#mycarousel").carousel('pause');
+    //         $("#carouselButton").children("span").removeClass('fa-pause');
+    //         $("#carouselButton").children("span").addClass('fa-play');
+    //     }
+    //     else if ($("#carouselButton").children("span").hasClass('fa-play')){
+    //         $("#mycarousel").carousel('cycle');
+    //         $("#carouselButton").children("span").removeClass('fa-play');
+    //         $("#carouselButton").children("span").addClass('fa-pause');                    
+    //     }
+    // });
 
     // Modal functions 
     $("#loginButton").click(function(){
